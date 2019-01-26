ed0b25fe5d6e23535b45b5e482c8ebbaa06ab140
3ppehr <sepehr.asghari1377@gmail.com>
2019-01-26 17:45:33 +0330

    beta police

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject building;
+    public static bool PoliceStartTimer=false;
+    public GameObject car;
+    public static bool PoliceAvailable = true;
+    public static bool firefighter = true;
+    public static bool tecnic = true;
+    public static bool ambulance = true;
+    public float time;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(PoliceStartTimer==true)
+        {
+            Police.WorkTime = Time.deltaTime + Police.WorkTime;
+            time = Time.deltaTime + Police.WorkTime;
+            if (Police.WorkTime >= 15)
+            {
+                
+                car.SetActive(true);
+                PoliceStartTimer = false;
+                PoliceAvailable = true;
+
+            }
+        }
+        
+	}
+}
---
+++ Assets/Script/Thief.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : MonoBehaviour {
+    public GameObject TheCar;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+    }
+    void OnColisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collision");
+        if (col.gameObject.tag =="Dozd")
+        {
+  
+            GameManager.PoliceAvailable = false;
+            GameManager.PoliceStartTimer = true;
+            TheCar.SetActive(false);
+        }
+  
+    }
+  
+}
